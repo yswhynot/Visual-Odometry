7f004a46719d9cdf8fcfc1e0b063dd28d8804c9e
Yisha <yswhynot@gmail.com>
2016-06-14 19:40:36 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,66 @@ const int IMG_HEIGHT = 600;
 
 void readme();
 
-void normalizeKeypoint(Point2f &p) {
-	p.x = (p.x - IMG_WIDTH / 2) / IMG_WIDTH * 2;
-	p.y = (p.y - IMG_HEIGHT / 2) / IMG_WIDTH * 2;
+void normalizeFeatures(std::vector<Point2f>& features, Mat& norm_t) {
+	norm_t = Mat::eye(3, 3, CV_64F);
+	norm_t.at<double> (0, 0) = 1 / (double) IMG_WIDTH * 2;
+	norm_t.at<double> (1, 1) = 1 / (double) IMG_HEIGHT * 2;
+	norm_t.at<double> (0, 2) = -0.5;
+	norm_t.at<double> (1, 2) = -0.5;
+
+	perspectiveTransform(features, features, norm_t);
+}
+
+void reconstruct3d(std::vector<KeyPoint>& keypoints,
+		std::vector<Point3f>& features3d, Mat& cam_intrinsic) {
+	std::vector<Point3f> features2d;
+	for (unsigned int i = 0; i < keypoints.size(); i++) {
+		Point3f tmp_p;
+		tmp_p.x = keypoints[i].pt.x;
+		tmp_p.y = keypoints[i].pt.y;
+		tmp_p.z = 1;
+		features2d.push_back(tmp_p);
+	}
+
+	Mat cam_trans_3d = Mat::zeros(4, 4, CV_64F);
+	Mat cam_inv = cam_intrinsic.inv();
+	cam_inv.copyTo(cam_trans_3d(Rect(0, 0, cam_inv.cols, cam_inv.rows)));
+	cam_trans_3d.at<double> (3, 3) = 1;
+
+	perspectiveTransform(features2d, features3d, cam_trans_3d);
+}
+
+void computeValidRtPair(std::vector<Point3f>& features3d, std::vector<Mat>& rt,
+		std::vector<double>& positive_percent, bool isFirst2Second) {
+	int max_rt_count = 0;
+	int selected_rt;
+	for (int i = 0; i < 4; i++) {
+		std::vector<Point3f> features3d_n;
+		unsigned int count = 0;
+		if (isFirst2Second)
+			perspectiveTransform(features3d, features3d_n, rt[i]);
+		else
+			perspectiveTransform(features3d, features3d_n, rt[i].inv());
+		for (unsigned int j = 0; j < features3d_n.size(); j++) {
+			if (features3d_n[j].z > 0)
+				count++;
+		}
+		positive_percent.push_back(
+				(double) count / (double) features3d_n.size());
+		if (count > max_rt_count) {
+			max_rt_count = count;
+			selected_rt = i;
+		}
+		if (isFirst2Second)
+			std::cout << "rt " << i << ": " << rt[i] << std::endl;
+		else
+			std::cout << "rt " << i << ": " << rt[i].inv() << std::endl;
+	}
+	for (int i = 0; i < 4; i++) {
+		printf("rt %d, percent positive: %f\n", i, positive_percent[i]);
+	}
+	printf("select rt: %d, percent positive: %f, count: %d\n", selected_rt,
+			positive_percent[selected_rt], max_rt_count);
 }
 
 int main(int argc, char** argv) {
@@ -37,7 +94,7 @@ int main(int argc, char** argv) {
 	cam_distortion.convertTo(cam_distortion, CV_64F);
 
 	Mat img_curr_origin, img_curr, img_prev_origin, img_prev;
-	img_prev_origin = imread("img/si0.jpg", CV_BGR2GRAY);
+	img_prev_origin = imread("img/s0.jpg", CV_BGR2GRAY);
 
 	// init the prev variables
 	std::vector<KeyPoint> keypoints_prev;
@@ -45,21 +102,25 @@ int main(int argc, char** argv) {
 	Mat descriptors_prev;
 
 	// init detection and extraction container
-	FastFeatureDetector detector(15);
+	FastFeatureDetector detector(80);
 	SurfDescriptorExtractor extractor;
 	FlannBasedMatcher matcher;
 
-	for (int img_i = 1; img_i < 7; img_i++) {
+	for (int img_i = 1; img_i < 13; img_i++) {
 		std::stringstream ss;
-		ss << "img/si" << img_i << ".jpg";
+		ss << "img/s" << img_i << ".jpg";
 		std::cout << "current idx: " << ss.str() << std::endl;
 		img_curr_origin = imread(ss.str(), CV_BGR2GRAY);
 
 		// init the img_prev parameters
-		if(img_i == 1) {
+		if (img_i == 1) {
 			undistort(img_prev_origin, img_prev, cam_intrinsic, cam_distortion);
 			detector.detect(img_prev, keypoints_prev);
 			extractor.compute(img_prev, keypoints_prev, descriptors_prev);
+			Mat img_keypoints_prev;
+			drawKeypoints(img_prev, keypoints_prev, img_keypoints_prev,
+					Scalar::all(-1), DrawMatchesFlags::DEFAULT);
+			imwrite("output/img/s0.jpg", img_keypoints_prev);
 		}
 
 		clock_t c_feature, c_extractor, c_match, c_homo;
@@ -110,16 +171,21 @@ int main(int argc, char** argv) {
 			//-- Get the keypoints from the good matches
 			Point2f kp_curr = keypoints_curr[good_matches[i].queryIdx].pt;
 			Point2f kp_prev = keypoints_prev[good_matches[i].trainIdx].pt;
-			normalizeKeypoint(kp_curr);
-			normalizeKeypoint(kp_prev);
 			good_curr.push_back(kp_curr);
 			good_prev.push_back(kp_prev);
 		}
 
 		Mat img_keypoints_curr;
 		drawKeypoints(img_curr, keypoints_curr, img_keypoints_curr,
-						Scalar::all(-1), DrawMatchesFlags::DEFAULT);
+				Scalar::all(-1), DrawMatchesFlags::DEFAULT);
 		imshow("Features", img_keypoints_curr);
+		std::string output = "output/";
+		output += ss.str();
+		imwrite(output, img_keypoints_curr);
+
+		Mat norm_t;
+		normalizeFeatures(good_prev, norm_t);
+		normalizeFeatures(good_curr, norm_t);
 
 		try {
 			// step 4: find fundamental Mat and rectification
@@ -135,6 +201,14 @@ int main(int argc, char** argv) {
 			Fw.at<double> (0, 0) = Fw_value.at<double> (0, 0);
 			Fw.at<double> (1, 1) = Fw_value.at<double> (0, 1);
 			Mat F_norm = Fu * Fw * Fvt;
+			F_norm = norm_t.t() * F_norm * norm_t;
+			std::cout << "good_prev: " << good_prev << std::endl;
+			std::cout << "good_curr: " << good_curr << std::endl;
+
+			F_norm = F_norm / norm(F_norm);
+			if (F_norm.at<double> (2, 2) < 0)
+				F_norm = -F_norm;
+			std::cout << "F_norm: " << F_norm << std::endl;
 
 			// step 6: svd
 			Mat E = cam_intrinsic.t() * F_norm * cam_intrinsic;
@@ -162,12 +236,7 @@ int main(int argc, char** argv) {
 			Mat t2 = -u.col(2);
 
 			// step 7: compute R-t, rescale t
-			double scale = sqrt((good_curr.back().y - good_curr.front().y) * (good_curr.back().y - good_curr.front().y)
-					+ (good_curr.back().x - good_curr.front().x) * (good_curr.back().x - good_curr.front().x))
-					/ sqrt((good_prev.back().y - good_prev.front().y) * (good_prev.back().y - good_prev.front().y)
-							+ (good_prev.back().x - good_prev.front().x) * (good_prev.back().x - good_prev.front().x));
-//			t1 = t1 / scale;
-//			t2 = t2 / scale;
+
 
 			std::vector<Mat> rt;
 			Mat rt_test = Mat::zeros(4, 4, CV_64F);
@@ -186,54 +255,19 @@ int main(int argc, char** argv) {
 			t2.copyTo(rt[3](Rect(3, 0, t2.cols, t2.rows)));
 
 			// step 8: reconstruct 3d
-			std::vector<Point3f> features2d, features3d;
-
-			for (unsigned int i = 0; i < keypoints_curr.size(); i++) {
-				Point3f tmp_p;
-				tmp_p.x = keypoints_curr[i].pt.x;
-				tmp_p.y = keypoints_curr[i].pt.y;
-				tmp_p.z = 1;
-				features2d.push_back(tmp_p);
-			}
-
-			Mat cam_trans_3d = Mat::zeros(4, 4, CV_64F);
-			Mat cam_inv = cam_intrinsic.inv();
-			cam_inv.copyTo(cam_trans_3d(Rect(0, 0, cam_inv.cols, cam_inv.rows)));
-			cam_trans_3d.at<double> (3, 3) = 1;
-
-			perspectiveTransform(features2d, features3d, cam_trans_3d);
+			std::vector<Point3f> features3d_curr, features3d_prev;
+			reconstruct3d(keypoints_prev, features3d_prev, cam_intrinsic);
+			reconstruct3d(keypoints_curr, features3d_curr, cam_intrinsic);
 
 			// step 9: select r-t pair
-			std::vector<double> positive_percent;
-			int max_rt_count = 0;
-			int selected_rt;
-			for (int i = 0; i < 4; i++) {
-				std::vector<Point3f> features3d_n;
-				unsigned int count = 0;
-				perspectiveTransform(features3d, features3d_n, rt[i]);
-				for (unsigned int j = 0; j < features3d_n.size(); j++) {
-					if (features3d_n[j].z > 0)
-						count++;
-				}
-				positive_percent.push_back(
-						(double) count / (double) features3d_n.size());
-				if (count > max_rt_count) {
-					max_rt_count = count;
-					selected_rt = i;
-				}
-				std::cout << "rt " << i << ": " << rt[i] << std::endl;
-			}
-			printf("Current idx: %d\n", img_i);
-			for (int i = 0; i < 4; i++) {
-				printf("rt %d, percent positive: %f\n", i, positive_percent[i]);
-			}
-			printf("select rt: %d, percent positive: %f, count: %d\n",
-					selected_rt, positive_percent[selected_rt], max_rt_count);
+			std::vector<double> percent_array_curr, percent_array_prev;
+			computeValidRtPair(features3d_prev, rt, percent_array_prev, true);
+			computeValidRtPair(features3d_curr, rt, percent_array_curr, false);
 
 		} catch (...) {
 		}
 
-		printf("Total time: %f seconds\n",
+		printf("Total time: %f seconds\n\n\n\n\n",
 				(float) (clock() - c_feature) / CLOCKS_PER_SEC);
 
 		// update all prev parameters
